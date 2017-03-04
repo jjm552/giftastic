93120a58c85f48646cd85406487314ed3ef44364
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-03-04 15:08:32 -0600

    notaction added to js

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -1,12 +1,12 @@
 window.onload = function() {
 
 
-    var gifButtons = [
+    var gifButtons = [                  //<--predefined array of terms used to create buttons
         "Whale", "Dolphin", "Seal", "Star Fish", "Shark", "Patrick", "Sponge Bob"
     ];
 
-    function buttonBuild() {
-        $("#gifButtons").empty();
+    function buttonBuild() {            //<--- buttonBuild begins on page load and createts buttons from predefined giffButton array
+        $("#gifButtons").empty();       // buttonBuild will also be called again when user adds to the array
         for (var i = 0; i < gifButtons.length; i++) {
             var a = $("<button>");
             a.addClass("animal");
@@ -16,20 +16,20 @@ window.onload = function() {
             $("#gifButtons").append(a);
         };
 
-        $("button").on("click", function() {
-            event.preventDefault();
-            $("#displayGifs").empty();
+        $("button").on("click", function() {        //<-- click event wating for user to click on one of the predefined buttons or
+            event.preventDefault();                 // user added buttons. Once button is clicked data from the button is used in the
+            $("#displayGifs").empty();              // api query to seach for the animal associated with the button
             var animal = $(this).attr("data-name");
             // console.log(animal);
             var rating = "";
             var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-            $.ajax({
+            $.ajax({                                //<-- ajax call to giphy to return 10 gifs defined by the button clicked 
                     url: queryURL,
                     method: "GET"
                 })
-                .done(function(response) {
-                    var results = response.data;
+                .done(function(response) {         //<--once objects are retuned from giphy divs are dynamically built to display
+                    var results = response.data;    // gifs in their still state
                     for (var i = 0; i < results.length; i++) {
                         var gifDiv = $("<div class='col-md-8'>");
                         var rating = results[i].rating;
@@ -49,8 +49,8 @@ window.onload = function() {
                         $("#displayGifs").prepend(gifDiv);
                     }
 
-                    $("img").on("click", function() {
-                        event.preventDefault();
+                    $("img").on("click", function() {       //<--click event to change the click image state to animate and back to
+                        event.preventDefault();             // still if click again
                         var state = $(this).attr("data-state")
                         // console.log(state);
                         if (state == "still") {
@@ -69,12 +69,12 @@ window.onload = function() {
         });
     };
 
-    $("#addUserInput").on("click", function() {
+    $("#addUserInput").on("click", function() {             //<--takes value enterd by user and addes to gifButtons array
         event.preventDefault();
         var userAnimal = $("#userInput").val().trim();
         gifButtons.push(userAnimal);
         buttonBuild();
     });
 
-    buttonBuild();
+    buttonBuild();                                          //<--on window load buttonBuild function is called to build buttons
 }
